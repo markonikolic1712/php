a07c4b46130d86d67933247f91f28b64ee0fa67a
markonikolic1712 <nikolic.marko11@gmail.com>
2017-12-16 19:17:13 +0100

    h

--- 2017.12.06/pcex10/whitelist/index.php
+++ 2017.12.06/pcex10/whitelist/index.php
@@ -1,5 +1,28 @@
 <?php
-$page = $_GET['page'];
-$pages = array("index.html","index.php","index.htm");
-if(in_array($page,$pages))
-    include "pages/" .  $page;
+$arr1 = ['a','b','c','d'];
+$arr2 = ['aaa','bbb','ccc','ddd'];
+$arr3 = [3,8,2,5,9];
+
+$arr_rez = array_combine($arr1,$arr2);
+
+
+//print_r($arr_rez);
+
+$arr3 = range(1,10);
+print_r($arr3);
+
+shuffle($arr3);
+print_r($arr3);
+
+sort($arr3);
+print_r($arr3);
+
+
+
+
+
+
+
+
+
+

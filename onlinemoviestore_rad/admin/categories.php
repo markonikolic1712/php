b6e2e812799ae1bf0e511c856a53c0063b50a59a
markonikolic1712 <nikolic.marko11@gmail.com>
2017-12-31 12:44:03 +0100

    h

--- onlinemoviestore_rad/admin/categories.php
+++ onlinemoviestore_rad/admin/categories.php
@@ -1,17 +1,46 @@
 <?php
-	print_r($_POST);
+require "../config.php";
+$conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DB);
+$selected_id = -1;
+$selected_name = "";
+$seleted_description = "";
+
+if(isset($_GET['cid'])){
+	$q = mysqli_query($conn, "select * from categories where id = {$_GET['cid']}");
+	$rw = mysqli_fetch_object($q);
+	if($rw){
+		$selected_id = $rw->id;
+		$selected_name = $rw->name;
+		$selected_description = $rw->description;
+	}
+}
+
+if(isset($_POST['btn_insert'])){
+	$selected_name = $_POST['tb_name'];
+	$selected_description = $_POST['tb_description'];
+	mysqli_query("insert into categories values (null, '{$selected_name}', '{$selected_description}')");
+	$selected_id = mysqli_insert_id($conn);
+	}
 ?>
 Categories
 <form method="post" action="">
-<select name="selCategory">
-	<option value="1">Select category</option>
+<?php
+$q = mysqli_query($conn, "select * from categories");
+?>
+<select onchange="window.location='?cid='+this.value" name="selCategory">
+	<option value="-1">Select category</option>
+<?php
+while($rw = mysqli_fetch_object($q)){
+	echo "<option ".($selected_id==$rw->id?"selected":"")."value='{$rw->id}'>{$rw->name}</option>";
+    }
+?>
 </select>
 <br>
 Name:<br>
-<input type="text" name="tb_name">
+<input type="text" name="tb_name" value="<?php echo $selected_name; ?>">
 <br>
 Description:<br>
-<input type="text" name="tb_description">
+<input type="text" name="tb_description" value="<?php echo $selected_description; ?>">
 <br><br>
 <input type="submit" name="btn_insert" value="Add new">
 <input type="submit" name="btn_update" value="Update">
--- onlinemoviestore_rad/index.php
+++ onlinemoviestore_rad/index.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysqli_connect("localhost","root","","moviestore");
+require "config.php";
+$conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DB);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -20,7 +21,7 @@ $conn = mysqli_connect("localhost","root","","moviestore");
     <dl id="browse">
       <dt>Full Category Lists</dt>
       <?php
-      $conn = mysqli_connect("localhost","root","","moviestore");
+
       $q = mysqli_query($conn, "select * from categories");
       while($rw = mysqli_fetch_object($q)){
         ?>
